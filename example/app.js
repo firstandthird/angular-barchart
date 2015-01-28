6eb848a67c08ada74e09496f7703c286d57afff0
Antonio Laguna <antonio@potatolondon.com>
2015-01-28 14:26:49 +0100

    Adding support for collections via attribute to
    indicate the key in which the value is.
    
    Fixes #1

--- example/app.js
+++ example/app.js
@@ -5,15 +5,24 @@ var exampleApp = angular.module('example-app',[
 
 exampleApp.controller('MainCtrl', function($scope) {
   $scope.mainVals = [4, 7, 9, 8, 12, 18, 20];
+  $scope.dictVals = [
+    { name: 'blah', value: 4 },
+    { name: 'blah2', value: 7 },
+    { name: 'blah3', value: 9 },
+    { name: 'blah4', value: 8 },
+    { name: 'blah5', value: 12 },
+    { name: 'blah6', value: 18 },
+    { name: 'blah7', value: 20 }
+  ];
 
   $scope.testit = function() {
     var tempVals = [];
-    for(i=0; i<12; i++)
-    {
-      tempVals.push( Math.floor((Math.random() * 20) + 1) );
+
+    for(var i = 0; i < 7; i++) {
+      tempVals.push(Math.floor((Math.random() * 20) + 1));
+      $scope.dictVals[i].value = Math.floor((Math.random() * 20) + 1);
     }
 
     $scope.mainVals = tempVals;
   }
-
 });
